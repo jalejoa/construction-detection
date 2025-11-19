4b1651defc16390b03c20803d2f30203fa7ed9e0
Alejandro <42ayja1mpg@hft-stuttgart.de>
2025-11-19 01:17:44 +0100

    Initial commit of project files

---
+++ data/src/config.py
@@ -0,0 +1,8 @@
+import os
+
+PLANET_API_KEY = os.getenv("PL_API_KEY")  # desde tu .env
+
+DATA_RAW = "data/raw/"
+DATA_PROCESSED = "data/processed/"
+DATA_QUADS = "data/quads/"
+DATA_MODELS = "data/models/"
